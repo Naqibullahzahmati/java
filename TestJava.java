41de86a715c849de87dbf845742d559b182dbf07
naqibullahzahmati <106928322+Naqibullahzahmati@users.noreply.github.com>
2022-10-22 04:00:35 -0700

    first file
    this file is about declaration and intialization

---
+++ TestJava.java
@@ -0,0 +1,24 @@
+
+package test.java;
+
+public class TestJava {
+
+    public static void main(String [] args){
+       int x;
+       x=10000;
+       System.out.println(x);
+       char y='$';
+       System.out.println(y);
+       float z =10.2f;
+       System.out.println(z);
+       double e=12.1;
+       System.out.println(e);
+       
+       
+        
+     
+      
+        
+        
+    }
+}
